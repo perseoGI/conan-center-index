f1413c6d4ec4c9377f07d88f1b0f1aef2a7a491d
PerseoGI <perseog@jfrog.com>
2025-03-24 14:35:24 +0100

    Removed changes

--- recipes/sdl_image/all/conanfile.py
+++ recipes/sdl_image/all/conanfile.py
@@ -101,44 +101,47 @@ class SDLImageConan(ConanFile):
             self.requires("libwebp/1.3.2")
         if self.options.with_avif:
             self.requires("libavif/1.0.1")
-        if self.options.with_jxl:
-            self.requires("libjxl/0.11.1")
 
     def validate(self):
         if self.options.shared and not self.dependencies["sdl"].options.shared:
             raise ConanInvalidConfiguration(f"{self.ref} shared requires sdl shared")
+        # TODO: libjxl doesn't support conan v2(yet)
+        if self.options.with_jxl:
+            raise ConanInvalidConfiguration(f"{self.ref} doesn't support with_jxl (yet)")
 
     def build_requirements(self):
         self.tool_requires("cmake/[>=3.16 <4]")
+    def _patch_sources(self):
+        apply_conandata_patches(self)
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        apply_conandata_patches(self)
+        self._patch_sources()
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.cache_variables["SDL2IMAGE_VENDORED"] = False
-        tc.cache_variables["SDL2IMAGE_DEPS_SHARED"] = False
-        tc.cache_variables["SDL2IMAGE_SAMPLES"] = False
-        tc.cache_variables["SDL2IMAGE_AVIF"] = self.options.with_avif
-        tc.cache_variables["SDL2IMAGE_BMP"] = self.options.bmp
-        tc.cache_variables["SDL2IMAGE_GIF"] = self.options.gif
-        tc.cache_variables["SDL2IMAGE_JPG"] = self.options.with_libjpeg
-        tc.cache_variables["SDL2IMAGE_JXL"] = self.options.with_jxl
-        tc.cache_variables["SDL2IMAGE_LBM"] = self.options.lbm
-        tc.cache_variables["SDL2IMAGE_PCX"] = self.options.pcx
-        tc.cache_variables["SDL2IMAGE_PNG"] = self.options.with_libpng
-        tc.cache_variables["SDL2IMAGE_PNM"] = self.options.pnm
-        tc.cache_variables["SDL2IMAGE_QOI"] = self.options.qoi
-        tc.cache_variables["SDL2IMAGE_SVG"] = self.options.svg
-        tc.cache_variables["SDL2IMAGE_TGA"] = self.options.tga
-        tc.cache_variables["SDL2IMAGE_TIF"] = self.options.with_libtiff
-        tc.cache_variables["SDL2IMAGE_WEBP"] = self.options.with_libwebp
-        tc.cache_variables["SDL2IMAGE_XCF"] = self.options.xcf
-        tc.cache_variables["SDL2IMAGE_XPM"] = self.options.xpm
-        tc.cache_variables["SDL2IMAGE_XV"] = self.options.xv
-        tc.cache_variables["SDL2IMAGE_BACKEND_WIC"] = self.options.get_safe("wic")
-        tc.cache_variables["SDL2IMAGE_BACKEND_IMAGEIO"] = self.options.get_safe("imageio")
+        tc.variables["SDL2IMAGE_VENDORED"] = False
+        tc.variables["SDL2IMAGE_DEPS_SHARED"] = False
+        tc.variables["SDL2IMAGE_SAMPLES"] = False
+        tc.variables["SDL2IMAGE_AVIF"] = self.options.with_avif
+        tc.variables["SDL2IMAGE_BMP"] = self.options.bmp
+        tc.variables["SDL2IMAGE_GIF"] = self.options.gif
+        tc.variables["SDL2IMAGE_JPG"] = self.options.with_libjpeg
+        tc.variables["SDL2IMAGE_JXL"] = self.options.with_jxl
+        tc.variables["SDL2IMAGE_LBM"] = self.options.lbm
+        tc.variables["SDL2IMAGE_PCX"] = self.options.pcx
+        tc.variables["SDL2IMAGE_PNG"] = self.options.with_libpng
+        tc.variables["SDL2IMAGE_PNM"] = self.options.pnm
+        tc.variables["SDL2IMAGE_QOI"] = self.options.qoi
+        tc.variables["SDL2IMAGE_SVG"] = self.options.svg
+        tc.variables["SDL2IMAGE_TGA"] = self.options.tga
+        tc.variables["SDL2IMAGE_TIF"] = self.options.with_libtiff
+        tc.variables["SDL2IMAGE_WEBP"] = self.options.with_libwebp
+        tc.variables["SDL2IMAGE_XCF"] = self.options.xcf
+        tc.variables["SDL2IMAGE_XPM"] = self.options.xpm
+        tc.variables["SDL2IMAGE_XV"] = self.options.xv
+        tc.variables["SDL2IMAGE_BACKEND_WIC"] = self.options.get_safe("wic")
+        tc.variables["SDL2IMAGE_BACKEND_IMAGEIO"] = self.options.get_safe("imageio")
         tc.generate()
         cd = CMakeDeps(self)
         cd.generate()
@@ -175,36 +178,34 @@ class SDLImageConan(ConanFile):
         if not self.options.shared:
             self.cpp_info.set_property("cmake_target_aliases", ["SDL2_image::SDL2_image-static"])
         self.cpp_info.set_property("pkg_config_name", "SDL2_image")
+        # TODO: back to global scope in conan v2 once legacy generators removed
+        self.cpp_info.components["_sdl_image"].libs = [f"SDL2_image{lib_postfix}"]
+        self.cpp_info.components["_sdl_image"].includedirs.append(os.path.join("include", "SDL2"))
 
-        self.cpp_info.libs = [f"SDL2_image{lib_postfix}"]
-        self.cpp_info.includedirs.append(os.path.join("include", "SDL2"))
-
-        self.cpp_info.set_property("cmake_target_name", "SDL2_image::SDL2_image")
-        self.cpp_info.set_property("pkg_config_name", "SDL2_image")
-        self.cpp_info.requires = ["sdl::sdl"]
+        self.cpp_info.components["_sdl_image"].set_property("cmake_target_name", "SDL2_image::SDL2_image")
+        self.cpp_info.components["_sdl_image"].set_property("pkg_config_name", "SDL2_image")
+        self.cpp_info.components["_sdl_image"].requires = ["sdl::sdl"]
         if self.options.with_libtiff:
-            self.cpp_info.requires.append("libtiff::libtiff")
+            self.cpp_info.components["_sdl_image"].requires.append("libtiff::libtiff")
         if self.options.with_libjpeg:
-            self.cpp_info.requires.append("libjpeg::libjpeg")
+            self.cpp_info.components["_sdl_image"].requires.append("libjpeg::libjpeg")
         if self.options.with_libpng:
-            self.cpp_info.requires.append("libpng::libpng")
+            self.cpp_info.components["_sdl_image"].requires.append("libpng::libpng")
         if self.options.with_libwebp:
-            self.cpp_info.requires.append("libwebp::libwebp")
+            self.cpp_info.components["_sdl_image"].requires.append("libwebp::libwebp")
         if self.options.with_avif:
-            self.cpp_info.requires.append("libavif::libavif")
-        if self.options.with_jxl:
-            self.cpp_info.requires.append("libjxl::libjxl")
+            self.cpp_info.components["_sdl_image"].requires.append("libavif::libavif")
         if self.options.get_safe("imageio") and not self.options.shared:
-            self.cpp_info.frameworks = [
+            self.cpp_info.components["_sdl_image"].frameworks = [
                 "CoreFoundation",
                 "CoreGraphics",
                 "Foundation",
                 "ImageIO",
             ]
             if self.settings.os == "Macos":
-                self.cpp_info.frameworks.append("ApplicationServices")
+                self.cpp_info.components["_sdl_image"].frameworks.append("ApplicationServices")
             else:
-                self.cpp_info.frameworks.extend([
+                self.cpp_info.components["_sdl_image"].frameworks.extend([
                     "MobileCoreServices",
                     "UIKit",
                 ])
