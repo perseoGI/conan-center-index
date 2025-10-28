ed02f7d3595bb9a542212849b5301b331f7086c5
PerseoGI <perseog@jfrog.com>
2025-10-28 13:11:26 +0100

    Removed static-library type for now

--- recipes/librealsense/all/conanfile.py
+++ recipes/librealsense/all/conanfile.py
@@ -145,7 +145,7 @@ class LibrealsenseConan(ConanFile):
 
         # rsutils component
         if Version(self.version) >= "2.56.5":
-            self.cpp_info.components["rsutils"].type = "static-library"
+            # rsutils is a static "static-library"
             self.cpp_info.components["rsutils"].set_property("cmake_target_name", "realsense2::rsutils")
             self.cpp_info.components["rsutils"].libs = [f"rsutils{postfix}"]
             self.cpp_info.components["rsutils"].requires = ["nlohmann_json::nlohmann_json", "lz4::lz4"]
